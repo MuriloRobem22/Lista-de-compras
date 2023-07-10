43c5cd62d7089b2f4c46a532154ac423846d2f8e
MuriloRobem22 <murimagal@outlook.com>
2023-07-09 21:46:31 -0300

    Atualização de responsividade

--- main.js
+++ main.js
@@ -1,98 +1,60 @@
-//arrays das listas
-let graos = []
-let congelados = []
-let laticinios = []
+//elementos
+let item = document.querySelector('#itemInput')
+let quantidade = document.querySelector('#addQuant')
+let lista = document.querySelector('#lista')
+let botaoAdd = document.querySelector('#botaoAdd')
+let inputApagarItem = document.querySelector('#apagarItem')
+let botaoApagar = document.querySelector('#apagar')
+//listas
 let frutas = []
+let congelados = []
 let produtos = []
+let graos = []
+let laticinios = []
 
-//nome do item e a categoria dele
-let item = document.querySelector('#additem')
-let categoria = document.querySelector('#lista')
-let _compra
-let _compraClasse = document.querySelector('.compra')
-//
-let _registro = document.querySelector('#registro')
-let _apagar = document.querySelector('#apagar')
-let apagarItem = document.querySelector('#apagarItem')
-let quantidade = document.querySelector('#quantidade')
-let _apagado
+let itemApagado
 let x
+let _compra
 
-function construtorTable(_compra) {
-  _compra = String(`<h4> Esta é a sua lista de compras organizada: </h4>
-  
-  <table style='width: 100%'>
-  <tr>
-  <th>Grãos</th>
-  <th>Frutas</th>
-  <th>Laticinios</th>
-  <th>Congelados</th>
-  <th>Produtos de limpeza</th>
-  </tr>
-  <tr>
-  <td> ${graos.join('<br>')} </td>
-  
-  <td> ${frutas.join('<br> ')} </td>
-  
-  <td> ${laticinios.join('<br> ')} </td>
-  
-  <td> ${congelados.join('<br> ')} </td>
+function itemAdd() {
+  switch (lista.value) {
+    case 'graos':
+      graos.push(quantidade.value + ' - ' + item.value)
+      console.log(`Grãos: ${graos.join(', ')}`)
+      break
+    case 'frutas':
+      frutas.push(quantidade.value + ' - ' + item.value)
+      console.log(`Frutas: ${frutas.join(', ')}`)
+      break
+    case 'produtos':
+      produtos.push(quantidade.value + ' - ' + item.value)
+      console.log(`Produtos de limpesa: ${produtos.join(', ')}`)
+      break
+    case 'congelados':
+      congelados.push(quantidade.value + ' - ' + item.value)
+      console.log(`Congelados: ${congelados.join(', ')}`)
+      break
+    case 'laticinios':
+      laticinios.push(quantidade.value + ' - ' + item.value)
+      console.log(`Latícinios: ${laticinios.join(', ')}`)
+      break
 
-  <td> ${produtos.join('<br> ')} </td>
-  </tr>
-  
-  
-  </table>`)
-
-  document.querySelector('.compra').innerHTML = _compra
-
-  item.value = ''
-}
-
-function envio(categoria, item, _compra) {
-  if (categoria === 'graos') {
-    graos.push(quantidade.value + ' - ' + item)
-
-    console.log(`tabela graos: ${graos.join(', ')}`)
-  }
-  if (categoria === 'congelados') {
-    congelados.push(quantidade.value + ' - ' + item)
-
-    console.log(`tabela congelados: ${congelados}`)
-  }
-  if (categoria === 'frutas') {
-    frutas.push(quantidade.value + ' - ' + item)
-
-    console.log(`tabela frutas: ${frutas}`)
-  }
-  if (categoria === 'laticinios') {
-    laticinios.push(quantidade.value + ' - ' + item)
-
-    console.log(`tabela laticinios: ${laticinios}`)
-  }
-  if (categoria === 'produtos') {
-    produtos.push(quantidade.value + ' - ' + item)
-  }
-  if (item === '' && categoria === 'escolha') {
-    item.pop()
-    alert(`Nenhum produto foi reconhecido!!`)
+    default:
+      alert(`Você não escolheu uma categoria para o produto.`)
+      break
   }
 }
 
-function AddClasse() {
-  _compraClasse.classList.add('compraadd')
-}
+function apagaItem() {
+  itemApagado = inputApagarItem.value
 
-function apagar(x) {
-  _apagado = apagarItem.value
-
-  let ig = graos.findIndex(item => item.includes(_apagado))
+  let ig = graos.findIndex(item => item.includes(itemApagado))
   console.log(`${ig}`)
-  let ic = congelados.findIndex(item => item.includes(_apagado))
+  let ic = congelados.findIndex(item => item.includes(itemApagado))
   console.log(`${ic}`)
-  let fi = frutas.findIndex(item => item.includes(_apagado))
+  let fi = frutas.findIndex(item => item.includes(itemApagado))
   console.log(`${fi}`)
-  let il = laticinios.findIndex(item => item.includes(_apagado))
+  let il = laticinios.findIndex(item => item.includes(itemApagado))
   console.log(`${il}`)
 
   if (ig !== -1) {
@@ -110,15 +72,41 @@ function apagar(x) {
   alert(`'${x}' foi apagado`)
 }
 
-registro.onclick = function () {
-  envio(categoria.value, item.value)
-  construtorTable(_compra)
-  AddClasse()
+function tabela() {
+  _compra = String(`<h4> Esta é a sua lista de compras organizada: </h4>
+  
+  <table class='tabela' >
+  <tr>
+  <th>Grãos</th>
+  <th>Frutas</th>
+  <th>Laticinios</th>
+  <th>Congelados</th>
+  <th>Limpeza</th>
+  </tr>
+  <tr>
+  <td> ${graos.join('<br><br>')} </td>
+  
+  <td> ${frutas.join('<br><br>')} </td>
+  
+  <td> ${laticinios.join('<br><br>')} </td>
+  
+  <td> ${congelados.join('<br><br>')} </td>
+
+  <td> ${produtos.join('<br><br>')} </td>
+  </tr>
+  
+  
+  </table>`)
+
+  document.querySelector('.compra').innerHTML = _compra
 }
 
-_apagar.onclick = function () {
-  apagar(x)
-  console.log(`${categoria.value}: item apagado: ${_apagado}`)
-  alert(`Item:${_apagado}, foi apagado da categoria graos `)
-  construtorTable(_compra)
-}
+botaoApagar.addEventListener('click', () => {
+  apagaItem()
+  tabela()
+})
+
+botaoAdd.addEventListener('click', () => {
+  itemAdd()
+  tabela()
+})
